03e1611e694c25e0e201434c03e15cb017340fb4
mclJohn <vitor_macyel@hotmail.com>
2025-05-07 13:29:42 -0300

    Primeiro commit

---
+++ FlaskApi/src/app.py
@@ -0,0 +1,75 @@
+from datetime import datetime
+import click
+import sqlalchemy as sa 
+from flask import Flask, current_app
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+
+
+
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(sa.String, unique=True)
+    email: Mapped[str] = mapped_column(sa.String)
+
+    def __repr__(self) -> str:
+        return f"Post(id={self.id!r}, title={self.title!r}, author_id={self.author_id!r})"
+
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(sa.String, nullable=False)
+    body: Mapped[str] = mapped_column(sa.String, nullable=False)
+    created: Mapped[datetime] = mapped_column(sa.DateTime, server_default=sa.func.now())
+    autor_id: Mapped[int] = mapped_column(sa.ForeignKey("user.id"))
+
+
+
+@click.command('init-db')
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    global db
+    with current_app.app_context():
+        db.create_all()
+    
+    click.echo('Initialized the database.')
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        SQLALCHEMY_DATABASE_URI="sqlite:///blog.sqlite",
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+    
+    db.init_app(app)
+    app.cli.add_command(init_db_command)
+
+   # Importa e registra a blueprint
+    from src.controllers import user
+    app.register_blueprint(user.app)
+
+    return app
---
+++ FlaskApi/src/controllers/user.py
@@ -0,0 +1,69 @@
+from flask import Blueprint, request   
+from src.app import User, db
+from http import HTTPStatus
+from sqlalchemy import inspect
+
+
+app = Blueprint("user", __name__, url_prefix="/users")
+
+def _create_user():
+    data = request.json
+    user = User(username=data["username"])
+    db.session.add(user)
+    db.session.commit()
+
+
+def _list_users():
+    query = db.select(User)
+    users = db.session.execute(query).scalars()
+    return [
+        {
+            "id : user.id"
+            "username": user.username,
+        }
+        for user in users
+
+    ]
+
+
+@app.route('/', methods=["GET", "POST"])
+def handle_user():
+    if request.method == "POST":
+        _create_user()
+        return {"message": "User created"}, 201
+    else:
+        return {"message": ["Users"]}
+    
+@app.route('/<int:user_id>')
+def get_user(user_id):
+    user = db.get_or_404(User, user_id)
+    return{
+        "id": user. id,
+        "username": user.username,
+    }
+
+@app.route('/<int:user_id>', methods=["PATCH"])
+def update_user(user_id):
+    user = db.get_or_404(User, user_id)
+    data = request.json
+
+    mapper = inspect(User)
+    for column in mapper.attrs:
+        if column.key in data:
+            setattr(user, column.key, data[column.key])
+
+    db.session.commit()
+
+    return{
+        "id": user. id,
+        "username": user.username,
+    }
+
+
+@app.route('/<int:user_id>', methods=["DELETE"])
+def delete_user(user_id):
+    user = db.get_or_404(User, user_id)
+    db.session.delete(user)
+    db.session.commit
+
+    return None, HTTPStatus.NO_CONTENT
