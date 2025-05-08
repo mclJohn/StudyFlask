b79cf72f1ac3de6d4dd79414eac470ae941e7cdf
mclJohn <vitor_macyel@hotmail.com>
2025-05-08 00:29:29 -0300

    Descreva aqui o que foi alterado

--- FlaskApi/src/app.py
+++ FlaskApi/src/app.py
@@ -7,12 +7,14 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
+from flask_migrate import Migrate
 
 
 class Base(DeclarativeBase):
   pass
 
 db = SQLAlchemy(model_class=Base)
+migrate = Migrate()
 
 
 
@@ -21,9 +23,11 @@ class User(db.Model):
     id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
     username: Mapped[str] = mapped_column(sa.String, unique=True)
     email: Mapped[str] = mapped_column(sa.String)
+    active: Mapped[bool] = mapped_column(sa.Boolean, default=True)
+
 
     def __repr__(self) -> str:
-        return f"Post(id={self.id!r}, title={self.title!r}, author_id={self.author_id!r})"
+        return f"Post(id={self.id!r}, title={self.title!r}, author_id={self.author_id!r}, active={self.active!r})"
 
 
 class Post(db.Model):
@@ -67,6 +71,7 @@ def create_app(test_config=None):
     
     db.init_app(app)
     app.cli.add_command(init_db_command)
+    migrate.init_app(app, db)
 
    # Importa e registra a blueprint
     from src.controllers import user
